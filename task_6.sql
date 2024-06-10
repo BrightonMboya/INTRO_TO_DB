7e0773458d457635a5dbc8eb3761b816e0b3dcbb
brightonmboya <b.mboya@alustudent.com>
2024-06-10 13:39:44 +0200

    task_6

---
+++ task_6.sql
@@ -0,0 +1,12 @@
+-- Check if the database name is provided as an argument
+SET @db_name = '$DATABASE_NAME';
+
+-- Use the database
+USE @db_name;
+
+-- Insert multiple rows into the customer table
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES 
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
